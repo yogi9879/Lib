f650a1a29e2fa2007acdf69b73be36eade9f4c09
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-22 10:57:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
-form pandas import *
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def upload_file():
    return "Hi"
+@app.route('/upload')
+def upload_file1():
+   return render_template('upload.html')
 if __name__ == '__main__':
    app.run()
