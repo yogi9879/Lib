21bf8c25fe705eb825c074aac299638e9e080b63
yogesh <nakhateyogesh@gmail.com>
2018-10-22 13:49:08 +0530

    dfds

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/upload')
+def upload_file():
+   return "Hi"
+if __name__ == '__main__':
+   app.run()
